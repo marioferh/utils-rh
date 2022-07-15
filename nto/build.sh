97c42fefe0b2fe394fe47299c0573eb0f78d1998
Mario Fernandez <mariofer@redhat.com>
2022-07-15 11:36:28 +0200

    README files
    Signed-off-by: Mario Fernandez <mariofer@redhat.com>

--- nto/build.sh
+++ nto/build.sh
@@ -2,9 +2,9 @@
 
 set -v
 
-REGISTRY=quay.io
-ORG=mariofer
-repos_dir=./repos        # change this to point to your repos dir (provided in repos.tar.xz)
+REGISTRY=
+ORG=
+repos_dir=./repos        # change this to point to your repos dir
 nto_dir=./cluster-node-tuning-operator
 
 build_push() {
@@ -12,14 +12,10 @@ build_push() {
   make -C $nto_dir local-image-push IMAGE=$IMAGE 
 }
 
-ocp49_rhel84() {
-  local image_tag=4.12
+local image_tag=
 
-  IMAGE_BUILD_EXTRA_OPTS="-v=$repos_dir/rhel-8.4:/etc/yum.repos.d:z" \
-  IMAGE=$REGISTRY/$ORG/cluster-node-tuning-operator:$image_tag \
-  DOCKERFILE=Dockerfile.rhel8 \
-    build_push
-}
+IMAGE=$REGISTRY/$ORG/cluster-node-tuning-operator:$image_tag \
+DOCKERFILE=Dockerfile \
+  build_push
 
 #git clone https://github.com/openshift/$nto_dir
-ocp49_rhel84
